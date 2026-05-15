18a630b5875649f3413b65be779c6d1ce9a94a7f
j <152007219+libSixx@users.noreply.github.com>
2026-05-15 14:33:55 +0800

    Add multiple imports to main.py

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-
+import traceback
+import ssl
+import socket
+import select
+from cryptography import x509
+from cryptography.x509.oid import NameOID
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import certifi
+import subprocess
+import sys
+import platform
+from datetime import datetime, timedelta, timezone
+import dearpygui.dearpygui as dpg
+import win32gui
+import win32con
+import keyboard
+import time
+import json
+import os
+import requests
+import threading
+from concurrent.futures import ThreadPoolExecutor
+import shutil
+import glob
+from tkinter import Tk, filedialog
+import pyperclip
+import urllib3
+from collections import defaultdict
+import ctypes
+import ctypes.wintypes
